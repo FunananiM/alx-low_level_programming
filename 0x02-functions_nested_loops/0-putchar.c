a4a833dc6b25b1805b6d9aa88ba5a36752afb646
funi m <isheontask.com>
2022-09-13 16:26:28 -0700

    Prints putch

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - prints _Puchar
+ *
+ * Description: Prints _Puchar
+ *
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	printf("_putchar\n");
+
+	return (0);
+}
